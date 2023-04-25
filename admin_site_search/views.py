9ebe41e33d9328925d20d68c30f1c9a49a108bc3
Felix Schlegel <schlegel.felix@gmail.com>
2023-04-25 13:48:45 +0200

    Update views.py
    fix indentation

--- admin_site_search/views.py
+++ admin_site_search/views.py
@@ -132,36 +132,36 @@ class AdminSiteSearchView:
         return False
 
     def match_objects(self, query: str, model_class: Model, model_fields: List[Field]) -> QuerySet:
-    filters = Q()
+        filters = Q()
 
-    for field in model_fields:
-        filter_ = self.filter_field(query, field)
-        if filter_:
-            filters |= filter_
-        elif isinstance(field, models.ForeignKey) and hasattr(field.related_model, "_parler_meta"):
-            # The ForeignKey field is pointing to a translated model
-            translated_fields = field.related_model._parler_meta.get_translated_fields()
+        for field in model_fields:
+            filter_ = self.filter_field(query, field)
+            if filter_:
+                filters |= filter_
+            elif isinstance(field, models.ForeignKey) and hasattr(field.related_model, "_parler_meta"):
+                # The ForeignKey field is pointing to a translated model
+                translated_fields = field.related_model._parler_meta.get_translated_fields()
+                translated_filters = Q()
+                for field_name in translated_fields:
+                    filter_ = Q(**{f"{field.name}__translations__{field_name}__icontains": query})
+                    translated_filters |= filter_
+                filters |= translated_filters
+
+        if hasattr(model_class, "_parler_meta"):
+            # The model is translatable
+            translated_fields = model_class._parler_meta.get_translated_fields()
             translated_filters = Q()
             for field_name in translated_fields:
-                filter_ = Q(**{f"{field.name}__translations__{field_name}__icontains": query})
+                filter_ = Q(**{f"translations__{field_name}__icontains": query})
                 translated_filters |= filter_
-            filters |= translated_filters
-
-    if hasattr(model_class, "_parler_meta"):
-        # The model is translatable
-        translated_fields = model_class._parler_meta.get_translated_fields()
-        translated_filters = Q()
-        for field_name in translated_fields:
-            filter_ = Q(**{f"translations__{field_name}__icontains": query})
-            translated_filters |= filter_
-        results = model_class.objects.translated().filter(translated_filters)[:5]
-    else:
-        if filters:
-            results = model_class.objects.filter(filters)[:5]
+            results = model_class.objects.translated().filter(translated_filters)[:5]
         else:
-            results = model_class.objects.none()
+            if filters:
+                results = model_class.objects.filter(filters)[:5]
+            else:
+                results = model_class.objects.none()
 
-    return results
+        return results
 
 
     def filter_field(self, query: str, field: Field) -> Optional[Q]:
